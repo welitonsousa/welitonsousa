a6be3320b9aef73fec793aebb0e8fc32ff1c0e64
Weliton Sousa <welitonubuntu@gmail.com>
2022-11-18 10:41:21 -0300

    .

--- next.config.js
+++ next.config.js
@@ -2,10 +2,16 @@
 
 const nextConfig = {
   swcMinify: true,
+  
   // dir: "src",
   images: {
     unoptimized: true,
   },
+  "public": true,
+    "builds": [
+        { "src": "package.json", "use": "@now/next" },
+        { "src": "static/css/style.css", "use": "@now/static" }
+    ]
 }
 
 module.exports = nextConfig
