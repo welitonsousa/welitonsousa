35f29674d25f2eb34e456dee71e2bdce1af5ed90
Weliton Sousa <welitonubuntu@gmail.com>
2022-11-18 22:35:20 -0300

    .

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  assetPrefix: 'https://raw.githubusercontent.com/welitonsousa/welitonsousa/main/',
+  assetPrefix: 'https://raw.githubusercontent.com/welitonsousa/welitonsousa/main/docs/',
   images: {
     unoptimized: true,
   }, 
